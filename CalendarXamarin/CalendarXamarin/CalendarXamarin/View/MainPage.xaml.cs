fddaf8d373bd7e49aa2451d100eb54b5a9348fcb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-30 17:55:33 +0530

    XAMARIN-34384 - Sample and screenshot attached.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/View/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI.XamForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public partial class MainPage : ReactiveContentPage<ViewModel>
+    {
+        public MainPage(ViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}
